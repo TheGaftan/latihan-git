e3fad12ac4cf1782d2a61d54280f070d33c02f61
Guntoro Saputro <128135823+TheGaftan@users.noreply.github.com>
2023-04-17 22:41:16 +0700

    Add files via upload

---
+++ tugas pertemuan 5/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo "Nama Saya Guntoro Saputro
+        Saya tinggal di daerah priok
+        hal yang saya sukai adalah menghabiskan uang";
+    }
+}
